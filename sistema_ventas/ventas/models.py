37313accf49913fdfaa9c28e34685eb70db46598
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-09-25 16:29:40 -0500

    Se crea modelo de producto, cliente y venta

--- sistema_ventas/ventas/models.py
+++ sistema_ventas/ventas/models.py
@@ -1,3 +1,30 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+# modelo de producto
+class Producto(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    precio = models.DecimalField(decimal_places=2)
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+# modelo de cliente
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nombre
+
+# modelo de Venta
+class Venta(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    fecha = models.DateTimeField()
+    cantidad = models.IntegerField()
+
+    def __str__(self):
+        return f"Venta de {self.cantidad} {self.producto.nombre} a {self.cliente.nombre}"
