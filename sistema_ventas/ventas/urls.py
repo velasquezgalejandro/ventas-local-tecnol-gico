a29c7fa80f91c250f28da39f37dbdd0f7be67bda
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-09-27 11:39:12 -0500

    Creacion de rutas para vista de clientes, productos y ventas

--- sistema_ventas/ventas/urls.py
+++ sistema_ventas/ventas/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import inicio
+from .views import inicio, clientes, productos, ventas
 
 urlpatterns = [
     path('', inicio, name='inicio'),  # Ruta para la página de inicio
+    path('clientes/', clientes, name='clientes'),  # Ruta para la página de clientes
+    path('productos/', productos, name='productos'),  # Ruta para la página de productos
+    path('ventas/', ventas, name='ventas'),  # Ruta para la página de ventas
 ]
--- sistema_ventas/ventas/views.py
+++ sistema_ventas/ventas/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def inicio(request):
     return render(request, 'inicio.html')
+
+# vista para acciones de productos
+def productos(request):
+    return render(request, 'productos.html')
+
+# vista para acciones de clientes
+def clientes(request):
+    return render(request, 'clientes.html')
+
+# vista para acciones de ventas
+def ventas(request):
+    return render(request, 'ventas.html')
