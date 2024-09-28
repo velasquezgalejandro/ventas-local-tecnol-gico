da985fead9c9fdf28903b8dce5b8da315a46a3f9
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-09-28 13:59:02 -0500

    se crean condiciones en form para la validacion en clientes

--- sistema_ventas/ventas/forms.py
+++ sistema_ventas/ventas/forms.py
@@ -20,7 +20,7 @@ class CategoriaForm(forms.ModelForm):
         if not nombre:
             raise forms.ValidationError('El nombre de la categoria es obligatorio.')
         if len(nombre) < 3:
-            raise forms.ValidationError('El nombre de la categoria debe tener al menos 4 caracteres.')
+            raise forms.ValidationError('El nombre de la categoria debe tener al menos 3 caracteres.')
         return nombre
 
 
@@ -29,7 +29,47 @@ class CategoriaForm(forms.ModelForm):
 class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
-        fields = ['nombre' , 'telefono' ,'email' ,'direccion']
+        fields = ['nombre', 'email', 'telefono', 'direccion']
+
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get('nombre')
+        if not nombre:
+            raise forms.ValidationError('El nombre de la categoria es obligatorio.')
+        if len(nombre) < 3:
+            raise forms.ValidationError('El nombre de la categoria debe tener al menos 3 caracteres.')
+        return nombre
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if not email:
+            raise forms.ValidationError('El correo electrónico es obligatorio.')
+
+        # Verificar el formato del correo electrónico
+        try:
+            forms.EmailField().clean(email)
+        except forms.ValidationError:
+            raise forms.ValidationError('El correo electrónico no es válido.')
+
+        return email
+
+    def clean_telefono(self):
+        telefono = self.cleaned_data.get('telefono')
+
+        if not telefono:
+            raise forms.ValidationError('El número de teléfono es obligatorio.')
+
+        if len(telefono) < 7 or not telefono.isdigit():
+            raise forms.ValidationError('El número de teléfono debe contener al menos 7 dígitos y solo números.')
+        return telefono
+
+    def clean_direccion(self):
+        direccion = self.cleaned_data.get('direccion')
+        if not direccion:
+            raise forms.ValidationError('La dirección es obligatoria.')
+        if len(direccion) < 10:
+            raise forms.ValidationError('La dirección debe tener al menos 10 caracteres.')
+        return direccion
 
 ### -------------------------------------------- ###
 # ventas form
