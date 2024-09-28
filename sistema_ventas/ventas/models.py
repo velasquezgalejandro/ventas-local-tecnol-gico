9cf25a932db0b8708cffea2949b769b690b0c4fb
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-09-27 19:49:56 -0500

    se agrega telefono y direccion a modelo de cliente

--- sistema_ventas/ventas/models.py
+++ sistema_ventas/ventas/models.py
@@ -24,6 +24,8 @@ class Producto(models.Model):
 class Cliente(models.Model):
     nombre = models.CharField(max_length=200)
     email = models.EmailField()
+    telefono = models.CharField(max_length=15)
+    direccion = models.CharField(max_length=300)
 
     def __str__(self):
         return self.nombre
