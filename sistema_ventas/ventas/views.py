30b37a7ed6dae7862119ef241cf6de838b9e8860
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-09-28 14:00:14 -0500

    se crea views para listar, agregar, ver detalle, editar y eliminar clientes

--- sistema_ventas/ventas/views.py
+++ sistema_ventas/ventas/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import CategoriaForm
 from django.db.models import Q
 from django.core.paginator import Paginator
 
-from .models import Categoria
+from .models import Categoria, Cliente
+from .forms import CategoriaForm, ClienteForm
 
 # Create your views here.
 def inicio(request):
@@ -68,6 +68,58 @@ def eliminar_categoria(request, pk):
 # vista para acciones de clientes
 def clientes(request):
     return render(request, 'clientes.html')
+
+#lista
+def listar_clientes(request):
+    clientes_list = Cliente.objects.all()
+    query = request.GET.get('q')
+
+    if query:
+        clientes_list = clientes_list.filter(Q(nombre__icontains = query))
+
+    paginator = Paginator(clientes_list, 10)
+    page_number = request.GET.get('page')
+    clientes = paginator.get_page(page_number)
+
+    return render(request, 'clientes/listar_clientes.html', {'clientes': clientes})
+
+# agrega
+def agregar_clientes(request):
+    if request.method == 'POST':
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_clientes')
+    else:
+        form = ClienteForm()
+    return render(request, 'clientes/crear_clientes.html', {'form':form})
+
+
+# detalle
+def detalle_cliente(request, pk):
+    cliente = Cliente.objects.get(pk=pk)
+    return render(request, 'clientes/detalle_cliente.html', {'cliente': cliente})
+
+# edita
+def editar_cliente(request, pk):
+    cliente = Cliente.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = ClienteForm(request.POST, instance=cliente)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_clientes')
+    else:
+        form = ClienteForm(instance=cliente)
+    return render(request, 'clientes/editar_cliente.html', {'form': form})
+
+
+# elimina
+def eliminar_cliente(request, pk):
+    cliente = get_object_or_404(Cliente, pk=pk)
+    if request.method == 'POST':
+        cliente.delete()
+        return redirect('listar_clientes')
+    return render(request, 'clientes/eliminar_cliente.html', {'cliente': cliente})
 
 ### -------------------------------------------------------------------###
 # vista para acciones de productos
