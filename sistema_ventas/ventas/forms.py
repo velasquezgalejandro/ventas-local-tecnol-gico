0eef38fea2aa0fd6a2d1b0bb0a6b677bfd2dc58a
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-09-27 19:46:51 -0500

    se corrige archivo formularios y se agregan comentarios de separacion

--- sistema_ventas/ventas/forms.py
+++ sistema_ventas/ventas/forms.py
@@ -1,21 +1,38 @@
 from django import forms
 from .models import Producto, Categoria, Cliente, Venta
 
+### -------------------------------------------- ###
+# producto Form
 class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
-        fields = ['nombre', 'categoria', 'proveedor', 'etiquetas', 'cantidad', 'precio', 'descripcion']
+        fields = ['nombre', 'categoria', 'descripcion', 'precio', 'stock' ]
 
+### -------------------------------------------- ###
+# categoria Form
 class CategoriaForm(forms.ModelForm):
     class Meta:
         model = Categoria
         fields = ['nombre', 'descripcion']
 
+    def clean_titulo(self):
+        nombre = self.cleaned_data.get('nombre')
+        if not nombre:
+            raise forms.ValidationError('El nombre de la categoria es obligatorio.')
+        if len(nombre) < 3:
+            raise forms.ValidationError('El nombre de la categoria debe tener al menos 4 caracteres.')
+        return nombre
+
+
+### -------------------------------------------- ###
+# cliente Form
 class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
-        fields = ['nombre' ,'telefono' ,'email' ,'direccion']
+        fields = ['nombre' , 'telefono' ,'email' ,'direccion']
 
+### -------------------------------------------- ###
+# ventas form
 class VentasForm(forms.ModelForm):
     class Meta:
         model = Venta
